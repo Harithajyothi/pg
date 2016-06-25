c22864e75bcfc6e79b1d5c91dddd4e1d258e43ae
Harithajyothi <Harithajyothi@users.noreply.github.com>
2016-06-25 13:08:51 +0530

    Create armstrong or not.c

---
+++ armstrong or not.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,sum=0;temp,rem;
+clrscr();
+printf("\nenter the number:");
+scanf("%d',&num);
+if(num<=0)
+{
+printf("\nit is not a positive number");
+}
+temp=num;
+while(temp!=0)
+{
+rem=temp%10;
+sum=sum+rem*rem*rem;
+temp=temp/10;
+}
+if(num==sum)
+printf("\nthe number is armstrong number");
+else
+printf("\nthe number is not armstrong number");
+}
