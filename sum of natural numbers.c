33ffa49fd6acd11759a74936aeba010f8f13bd54
Harithajyothi <Harithajyothi@users.noreply.github.com>
2016-06-25 11:38:40 +0530

    Update sum of natural numbers.c

--- sum of natural numbers.c
+++ sum of natural numbers.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
+  clrscr();
 int i,sum=0,num;
 printf("\nenter the integer number);
 scanf("%d",&num);
@@ -13,5 +15,6 @@ for(i=1;i<=num;i++)
 sum=sum+i;
 }
 printf("\nthe sum of %d numbers=%d",num,sum);
+getch();
 }
 
