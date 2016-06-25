a28af448153f4a8db3ad155e09ffcdc646b6d371
Harithajyothi <Harithajyothi@users.noreply.github.com>
2016-06-25 11:27:44 +0530

    Create sum of natural numbers.c

---
+++ sum of natural numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,sum=0,num;
+printf("\nenter the integer number);
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("\nthe sum of %d numbers=%d",num,sum);
+}
+
