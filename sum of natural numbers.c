9f9dd8b49cc2f1853d064e657f5556ba7abe6f33
Harithajyothi <Harithajyothi@users.noreply.github.com>
2016-06-25 11:37:12 +0530

    Update sum of natural numbers.c

--- sum of natural numbers.c
+++ sum of natural numbers.c
@@ -6,6 +6,10 @@ printf("\nenter the integer number);
 scanf("%d",&num);
 for(i=1;i<=num;i++)
 {
+  if(i<0)
+  {
+    printf("\nentered number is not natural number");
+  }
 sum=sum+i;
 }
 printf("\nthe sum of %d numbers=%d",num,sum);
